2fad7d1fdb723b0e8487b5297864f7ed5fedc367
saasan <saasan.s2works@hotmail.com>
2022-05-07 21:51:49 +0000

    Copy Bookmarklet

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { minify } from 'uglify-js';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bookmarkletify" is now active!');
+    let disposable = vscode.commands.registerCommand('bookmarkletify.copyBookmarklet', () => {
+		const editor = vscode.window.activeTextEditor;
+        if (editor?.document?.languageId !== 'javascript') {
+            return;
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bookmarkletify.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Bookmarkletify!');
+        let text = editor.document.getText();
+        text = text.trim();
+        if (text.length === 0) {
+            return;
+        }
+
+        if (text.startsWith('javascript:')) {
+            text = text.substring('javascript:'.length);
+        }
+
+        const minified = minify(text);
+        if(minified.error) {
+            vscode.window.showErrorMessage(minified.error.message);
+            return;
+        }
+
+        text = 'javascript:' + encodeURIComponent('(()=>{' + minified.code + '})();');
+
+        vscode.env.clipboard.writeText(text);
+        vscode.window.showInformationMessage('Bookmarkletify: Copied!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
